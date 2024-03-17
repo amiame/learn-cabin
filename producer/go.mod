225713654369709900c2372a8250f5b12f97f205
amiame <amiame2008@gmail.com>
2024-03-17 23:49:34 +0900

    implemented a watcher that sends messages to the consumer when there is a change in the policy

--- producer/go.mod
+++ producer/go.mod
@@ -2,9 +2,13 @@ module test-casbin
 
 go 1.21.1
 
-require github.com/segmentio/kafka-go v0.4.47
+require (
+	github.com/casbin/casbin/v3 v3.0.0-beta.7
+	github.com/segmentio/kafka-go v0.4.47
+)
 
 require (
+	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/stretchr/testify v1.8.2 // indirect
--- producer/main.go
+++ producer/main.go
@@ -8,18 +8,61 @@ import (
 	"os"
 	"time"
 
+	"github.com/casbin/casbin/v3"
 	kafka "github.com/segmentio/kafka-go"
 )
 
+var _enforcer *casbin.Enforcer
+var _watcher *watcher
+
 func main() {
+	setupCasbin()
+
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Press Enter to simulate casbin policy update")
-		reader.ReadString('\n')
-		produce()
+		if _, err := reader.ReadString('\n'); err != nil {
+			log.Fatal("failed to read string:", err)
+		}
+
+		editPolicy()
+
 		fmt.Print("A message has been sent to all MS telling them that casbin policy has been updated\n")
 		time.Sleep(time.Second * 2)
 		fmt.Print("\n")
+	}
+}
+
+func setupCasbin() {
+	e, err := casbin.NewEnforcer("../config/rbac_model.conf", "../config/initial_policy.csv")
+	if err != nil {
+		log.Fatal("failed to create enforcer:", err)
+	}
+
+	_enforcer = e
+	_watcher = &watcher{}
+
+	if err := _enforcer.SetWatcher(_watcher); err != nil {
+		log.Fatal("failed to set watcher:", err)
+	}
+}
+
+func editPolicy() {
+	subject := time.Now().String()
+	object := "xxx-xxxApi"
+	action := "use"
+
+	added, err := _enforcer.AddPolicy(subject, object, action)
+	if err != nil {
+		log.Fatal("failed to add authorization rule:", err)
+	}
+
+	if !added {
+		log.Fatal("new authorization rule was not added")
+	}
+
+	if err := _enforcer.SavePolicy(); err != nil {
+		log.Fatal("failed to save policy:", err)
 	}
 }
 
@@ -33,7 +76,9 @@ func produce() {
 		log.Fatal("failed to dial leader:", err)
 	}
 
-	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	if err := conn.SetWriteDeadline(time.Now().Add(10 * time.Second)); err != nil {
+		log.Fatal("failed to set write deadline:", err)
+	}
 	_, err = conn.WriteMessages(
 		kafka.Message{Value: []byte("please update your casbin enforcers!")},
 	)
