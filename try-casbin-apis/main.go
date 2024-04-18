31280eea792abe57a858d40c47fa1909d06559d0
amiame <amiame2008@gmail.com>
2024-04-18 18:11:03 +0900

    feat: add getUserRoles function

--- try-casbin-apis/main.go
+++ try-casbin-apis/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/casbin/casbin/v2"
 )
@@ -20,9 +21,46 @@ func main() {
 	//res, err := e.Enforce(enforceContext, "workbench", "alice", "survey-create", "survey/56")
 	e.SetFieldIndex("p", "app", 1)
 
+	getUserRoles(e, "app1", "doc", "doc1", "amir")
 	getRoleUsers(e, "app1", "doc", "doc1", "owner")
 	getObjectRoles(e, "app1", "doc", "doc1")
 
+}
+
+func getUserRoles(e *casbin.Enforcer, app string, object string, instance string, user string) {
+	// How to get roles of a user
+	// Given object ID, instance ID, and user ID
+	rm := e.GetRoleManager()
+	roles, err := rm.GetRoles(user, app)
+	if len(roles) == 0 {
+		fmt.Println("no roles")
+	}
+	exitIfError(err)
+
+	roleNames := make([]string, 0)
+	for _, r := range roles {
+		s1 := strings.Split(r, ":")
+		if s1[0] != object {
+			continue
+		}
+
+		s2 := strings.Split(s1[1], "-")
+		if s2[0] != instance {
+			continue
+		}
+
+		roleNames = append(roleNames, s2[1])
+	}
+
+	if len(roleNames) == 0 {
+		return
+	}
+
+	fmt.Printf("Roles of '%s' in '%s:%s' in '%s' are:\n", user, object, instance, app)
+	for _, r := range roleNames {
+		fmt.Printf("  - %s\n", r)
+	}
+	fmt.Println()
 }
 
 func getRoleUsers(e *casbin.Enforcer, app string, object string, instance string, role string) {
